48c2d783993a905913f7d864cac371753b15d292
Jaime Forero <j.e.forero.romero@gmail.com>
2017-02-21 15:30:52 -0500

    update main

---
+++ run.py
@@ -0,0 +1,39 @@
+import sys
+from matplotlib.dates import strpdate2num
+import numpy as np
+import  subprocess
+
+def main():
+    try:
+        fiberassign_dates = 'world/epochs_date.txt'
+        f = open(fiberassign_dates, 'r')
+    except IOError as e:
+        print('Problem opening file epochs_date.txt')
+        exit()
+    
+    dates = {}
+    fx = f.readlines()
+    for line in fx:
+        line = line.strip()
+        if line.startswith('#') or len(line) < 2:
+            continue
+        line = line.split()
+        epoch = int(line[0])
+        yearmmdd = [line[1], line[2]]
+        dates[epoch] = {}
+        dates[epoch]['BEGINS']= line[1]
+        dates[epoch]['ENDS']= line[2]
+
+    for k in dates.keys():
+        print(k)
+        make_process = subprocess.Popen("make EPOCH={};".format(k), shell=True, stdout=subprocess.PIPE, stderr=sys.stdout.fileno())
+        while True:
+            line = make_process.stdout.readline()
+            if not line:break
+            print(line), #output to console in time
+            sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    main()
+
